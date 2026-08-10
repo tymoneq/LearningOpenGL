4abaaf7b2321086490b7b63c3f9910f6ce222652
Tymon Tumialis <tymontumialis@gmail.com>
2026-08-10 17:53:00 +0200

    uniforms

--- main.cpp
+++ main.cpp
@@ -129,6 +129,8 @@ int main(int argc, char *argv[]) {
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  glfwSwapInterval(1);
+
   GLenum err = glewInit();
   if (GLEW_OK != err) {
     /* Problem: glewInit failed, something is seriously wrong. */
@@ -162,12 +164,25 @@ int main(int argc, char *argv[]) {
       CreateShader(source.VertexSource, source.FragmentSource);
   GLCall(glUseProgram(shader));
 
+  int location = glGetUniformLocation(shader, "u_Color");
+  ASSERT(location != -1);
+  GLCall(glUniform4f(location, 0.9f, 0.3f, 0.8f, 1.0f));
+
+  float r = 0.0f;
+  float increment = 0.05f;
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
-
+    GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+    if (r > 1.0f) {
+      increment = -0.05f;
+    } else if (r < 0.0f)
+      increment = 0.05f;
+
+    r += increment;
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
